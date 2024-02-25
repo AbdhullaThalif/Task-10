989951c9e8cf8a1d637b344e0e69228ebb78c533
AbdhullaThalif <158301949+AbdhullaThalif@users.noreply.github.com>
2024-02-26 05:22:06 +0530

    Add files via upload

---
+++ Task_10_5.java
@@ -0,0 +1,17 @@
+package programs;
+
+public class TeaClass {
+
+	public void prepareTea() {
+		System.out.println("A Basic tea with hot water and tea leaves");
+	}
+	
+	public void addMilk() {
+		System.out.println("Adds the milk to basic tea");
+	}
+	
+	public void addSugar() {
+		System.out.println("Adds the sugar to the basic tea");
+	}
+	
+}
